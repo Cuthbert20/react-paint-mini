c27676b614ef256bd44879a7445a6f1f42c98a15
Cuthbert20 <knowltonspencer@gmail.com>
2019-07-10 18:21:52 -0600

    exp

--- react-paint-mark/src/components/PaintCanvas.js
+++ react-paint-mark/src/components/PaintCanvas.js
@@ -1,3 +1,12 @@
 import React, {Component} from 'react'
 
-export default class PaintCanvas export
+export default class PaintCanvas extends Component {
+    constructor() {
+        super()
+    }
+    render(){
+        return(
+            
+        )
+    }
+}
