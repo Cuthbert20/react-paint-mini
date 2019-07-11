f1ee69684f19af6b105cb0565d3a81db7cb72b60
Cuthbert20 <knowltonspencer@gmail.com>
2019-07-10 18:04:35 -0600

    paint update

---
+++ react-paint-mark/src/components/Square.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+export default class Square extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div className="square"></div>
+        )
+    }
+}
